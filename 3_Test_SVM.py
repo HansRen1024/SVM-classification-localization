3fbb17b0645846fea907264e67399705d0e0c011
Hans Ren <171357351@qq.com>
2017-07-05 10:01:10 +0800

    Update and rename 3_Test_PCA+SVM.py to 3_Test_SVM.py

--- 3_Test_SVM.py
+++ 3_Test_SVM.py
@@ -1,21 +1,17 @@
 #!/usr/bin/env python2  
 # -*- coding: utf-8 -*-  
 """ 
-Created on Thu Jun 15 17:29:22 2017 
+Created on Thu Jun 15 16:44:53 2017 
  
 @author: hans 
 """  
-  
 from sklearn.externals import joblib  
 import glob  
 import os  
 import time  
   
-m = '20pixel'  
-n = 100  
-  
 if __name__ == "__main__":  
-    model_path = './models/%s/svm_%s_pca_%s.model' %(m,m,n)  
+    model_path = './models/svm.model'  
     test_feat_path = './features/test'  
     total=0  
     num=0  
@@ -26,12 +22,6 @@ if __name__ == "__main__":
         print "%d processing: %s" %(total, feat_path)  
         data_test = joblib.load(feat_path)  
         data_test_feat = data_test[:-1].reshape((1,-1))  
-#------------------------PCA--------------------------------------------------  
-        meanVal = joblib.load('./features/PCA/%s/meanVal_train_%s.mean' %(m,m))  
-        data_test_feat = data_test_feat - meanVal 
-        n_eigVects = joblib.load('./features/PCA/%s/n_eigVects_train_%s_%s.eig' %(m,m,n))  
-        data_test_feat = data_test_feat * n_eigVects 
-#------------------------PCA--------------------------------------------------  
         result = clf.predict(data_test_feat)  
         if int(result) == int(data_test[-1]):  
             num += 1  
