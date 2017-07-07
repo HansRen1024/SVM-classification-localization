87eca1e030a7936b3ec347b4c671e219b833a6df
Hans Ren <171357351@qq.com>
2017-07-07 09:32:03 +0800

    Update 6_PSO+PCA.py

--- 6_PSO+PCA.py
+++ 6_PSO+PCA.py
@@ -1,36 +1,37 @@
-#!/usr/bin/env python
-#encoding:utf-8
-
 from sklearn.externals import joblib
+import os
+#import sys
+#sys.path.append('/home/xuegengjian/renhanchi/libsvm-3.22/python')
 from svmutil import svm_train
 import numpy as np
-import os
 import glob
 import random
 import copy
 
-n = 100 # how many dimention do you want 
-m = '20pixel' # if m made u confused, u can delete all about m. it is not necessary.
+n = 500
 train_feat_path = './features/train'
 
-birds = 30 # size of population
+birds = 20 # size of population
 maxgen = 50
-pos = []
+pos = [] # population of class
 speed = []
 bestpos = []
+initpos = []
 birdsbestpos = []
 fds = []
 dict_fds = []
 labels = []
-w = 0.8 # best belongs to [0.8,1.2]
+allbestpos = []
+w = 1 # best belongs to [0.8,1.2]
 c1 = 2
 c2 = 2
 r1 = random.uniform(0,1)
 r2 = random.uniform(0,1)
+m = 'pso'
 
 def zeroMean(dataMat):
     meanVal=np.mean(dataMat,axis=0)
-    joblib.dump(meanVal,'./features/PCA/%s/meanVal_train_%s.mean' %(m,m))
+#    joblib.dump(meanVal,'./features/PCA/meanVal_train_%s.mean' %m)
     newData=dataMat-meanVal
     return newData,meanVal
 
@@ -38,18 +39,18 @@ def pca(dataMat,n):
     print "Start to do PCA..."
     newData,meanVal=zeroMean(dataMat)
     
-    covMat=np.cov(newData,rowvar=0)
-    eigVals,eigVects=np.linalg.eig(np.mat(covMat))
-    joblib.dump(eigVals,'./features/PCA/%s/eigVals_train_%s.eig' %(m,m),compress=3)
-    joblib.dump(eigVects,'./features/PCA/%s/eigVects_train_%s.eig' %(m,m),compress=3)
+#    covMat=np.cov(newData,rowvar=0)
+#    eigVals,eigVects=np.linalg.eig(np.mat(covMat))
+#    joblib.dump(eigVals,'./features/PCA/eigVals_train_%s.eig' %m,compress=3)
+#    joblib.dump(eigVects,'./features/PCA/eigVects_train_%s.eig' %m,compress=3)
     
-#    eigVals = joblib.load('./features/PCA/%s/eigVals_train_%s.eig' %(m,m))
-#    eigVects = joblib.load('./features/PCA/%s/eigVects_train_%s.eig' %(m,m))
+    eigVals = joblib.load('./features/PCA/eigVals_train_%s.eig' %m)
+    eigVects = joblib.load('./features/PCA/eigVects_train_%s.eig' %m)
 
     eigValIndice=np.argsort(eigVals)
     n_eigValIndice=eigValIndice[-1:-(n+1):-1]
     n_eigVect=eigVects[:,n_eigValIndice]
-    joblib.dump(n_eigVect,'./features/PCA/%s/n_eigVects_train_%s_%s.eig' %(m,m,n))
+#    joblib.dump(n_eigVect,'./features/PCA/n_eigVects_train_%s_%s.eig' %(m,n))
     lowDDataMat=newData*n_eigVect
     return lowDDataMat
 
@@ -69,19 +70,23 @@ for i in range(birds):
     pos.append([])
     speed.append([])
     bestpos.append([])
+    initpos.append([])
 
 def CalDis(list):
     fitness=0.0
-    param = '-t 2 -v 3 -c %s -g %s' %(list[0],list[1]) # -v decides n-fold cross validation
-    fitness = svm_train(labels, dict_fds, param) 
-    return -fitness
+    param = '-t 2 -v 3 -c %s -g %s' %(list[0],list[1])
+    fitness = svm_train(labels, dict_fds, param)
+    return fitness
 
 for i in range(birds):          #initial all birds' pos,speed
-    pos[i].append(random.uniform(1,100))
-    pos[i].append(random.uniform(0,0.0001)) # maximum 1/num_features
-    speed[i].append(random.uniform(-10,10))
-    speed[i].append(random.uniform(-0.00001,0.00001))
+    pos[i].append(random.uniform(10,20))
+    pos[i].append(random.uniform(0.9e-06, 1.5e-06)) # 1/num_features
+    speed[i].append(float(0))
+    speed[i].append(float(0))
+#    speed[i].append(random.uniform(-10,10))
+#    speed[i].append(random.uniform(-0.00002,0.00002))
     bestpos[i] = copy.deepcopy(pos[i])
+    initpos[i] = copy.deepcopy(pos[i])
 
 def FindBirdsMostPos():
     best=CalDis(bestpos[0])
@@ -89,14 +94,14 @@ def FindBirdsMostPos():
     for i in range(birds):
         print "\n>>>>>The %d'd time to find globel best pos. Total %d times.\n" %(i+1, birds)
         temp = CalDis(bestpos[i])
-        if temp < best:
+        if temp > best:
             best = temp
             index = i
-            print index
-    return bestpos[index]
+            print '------- %d: %f' %(index, -best)
+    return best, bestpos[index]
 
 print "\n-------------------------Initial Globel Best Pos----------------------------------\n"
-birdsbestpos = FindBirdsMostPos()   #initial birdsbestpos
+best_predict, birdsbestpos = FindBirdsMostPos()   #initial birdsbestpos
 print "\n-------------------------Done Globel Best Pos----------------------------------\n"
 
 def NumMulVec(num,list):         #result is in list
@@ -131,13 +136,19 @@ def UpdatePos():
     for i in range(birds):
         if pos[i][0]+speed[i][0] > 0 and pos[i][1]+speed[i][1] > 0: 
             VecAddVec(pos[i],speed[i])
-            if CalDis(pos[i])<CalDis(bestpos[i]):
+            if CalDis(pos[i]) > CalDis(bestpos[i]):
                 bestpos[i] = copy.deepcopy(pos[i])
-    birdsbestpos = FindBirdsMostPos()
+    best_predict, birdsbestpos = FindBirdsMostPos()
     return birdsbestpos
 
-for i in range(maxgen):
-    print "\n>>>>>>>>The %d'd time to update parameters. Total %d times\n" %(i+1, maxgen)
+for asd in range(maxgen):
+    print "\n>>>>>>>>The %d'd time to update parameters. Total %d times\n" %(asd+1, maxgen)
     UpdateSpeed()
     best_para = UpdatePos()
-print "Ater %d iterations\nthe best C is: %f\nthe best gamma is: %f" %(maxgen,best_para[0],best_para[1])
+    
+    allbestpos.append(best_para)
+    f=open('result/PSO_%s-%s-%s.txt' %(birds,maxgen,n),'w')
+    f.write(str(allbestpos))
+    f.close()
+    
+print "After %d iterations\nthe best C is: %f\nthe best gamma is: %f" %(maxgen,best_para[0],best_para[1])
