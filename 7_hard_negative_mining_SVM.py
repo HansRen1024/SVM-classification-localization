b3a834b6eb0307efe9bf0b9562a14a1b7cbef655
Hans Ren <171357351@qq.com>
2017-07-07 10:21:39 +0800

    Update 7_hard_negative_mining_SVM.py

--- 7_hard_negative_mining_SVM.py
+++ 7_hard_negative_mining_SVM.py
@@ -5,39 +5,44 @@ import numpy as np
 import sklearn.svm as ssv
 from sklearn.externals import joblib
 from skimage.feature import hog
+import random
 import glob
 import os
 import cv2
 
 def trainSvm(datas, labels):
-#    clf = ssv.SVC(kernel='rbf', C=51.888660461910455, gamma=6.340844354646774e-05) #10,50,100
-    clf = ssv.SVC(kernel='rbf', C=36.076088, gamma=0.000075) #10,100,500
+    clf = ssv.SVC(kernel='rbf', C=17.255220940030252, gamma=1.2943653125547475e-06) #20,50,500,0.842850
     print "Training a SVM Classifier."
     clf.fit(datas, labels)
     return clf
 
 def loadData(path):
-    fds = []
-    labels = []
+    fds_all = []
+#    fds = []
+#    labels = []
     num=0
     for feat_path in glob.glob(os.path.join(path, '*.feat')):
         num += 1
         data = joblib.load(feat_path)
-        fds.append(data[:-1])
-        labels.append(data[-1])
+        fds_all.append(data)
+#        fds.append(data[:-1])
+#        labels.append(data[-1])
         print "%d Dealing with %s" %(num,feat_path)
-    return fds, labels
+    return fds_all
 
 def sliding_window(image, stepSize, windowSize):
-	# slide a window across the image
 	for y in xrange(0, image.shape[0], stepSize):
-		for x in xrange(0, image.shape[1], stepSize):
-			# yield the current window    
+		for x in xrange(0, image.shape[1], stepSize):   
 			yield (x, y, image[y:y + windowSize[1], x:x + windowSize[0]])
 
 def rgb2gray(im):
     gray = im[:, :, 0]*0.2989+im[:, :, 1]*0.5870+im[:, :, 2]*0.1140
     return gray
+
+def savefeat(childDir, num_win, fd):
+    fd_name = childDir.split('.')[0] + '_%d.feat' %num_win
+    fd_path = os.path.join('./features/train_hnm/', fd_name)
+    joblib.dump(fd, fd_path,compress=3)
 
 if __name__ == "__main__":
     normalize = True
@@ -46,16 +51,11 @@ if __name__ == "__main__":
     cells_per_block = [2,2]
     pixels_per_cell = [20,20]
     orientations = 9
-#    m = 'home'
-    new_model_path = './models/svm_h_p_m.model'
-    train_feat_path = './features/train'
-    fds, labels = loadData(train_feat_path) 
+    new_model_path = './models/svm_pso_hnm.model'
+    train_feat_path = './features/train_'
+    fds_all = loadData(train_feat_path) 
 
-
-#    clf = trainSvm(fds, labels)
-
-
-    model_path = './models/svm_20pixel.model'
+    model_path = './models/svm_pso.model'
     clf = joblib.load(model_path)
     
 #--------------------hard_negative_mining-------------------------------------
@@ -63,10 +63,11 @@ if __name__ == "__main__":
     num = 0
     for childDir in os.listdir(negative_img_path):
         num += 1
+        num_win = 0
         print "num: %d hard negative mining: %s" %(num,childDir)
         f = os.path.join(negative_img_path, childDir)
         data = cv2.imread(f)
-        scales = [(200,200), (300,300), (400,400), (500, 500), (600,600), (800, 800)]
+        scales = [(100, 100), (200,200), (300,300), (400,400), (500, 500), (600,600), (800, 800)]
         for (winW,winH) in scales:
             for (x, y, window) in sliding_window(data, stepSize=100, windowSize=(winW,winH)):
                 result = 0
@@ -76,12 +77,21 @@ if __name__ == "__main__":
                     window = cv2.resize(window,(200,200),interpolation=cv2.INTER_CUBIC)
                 gray = rgb2gray(window)/255.0
                 window_fd = hog(gray, orientations, pixels_per_cell, cells_per_block, block_norm, visualize, normalize)
-                window_fd.shape = 1,-1
-                result = int(clf.predict(window_fd))
+                win_fd = window_fd.reshape(1, -1)
+                result = int(clf.predict(win_fd))
                 if result == 1:
-                    window_fd.shape = (2916,)
-                    fds.append(window_fd)
-                    labels.append(float(0))
+                    num_win += 1
+                    fd = np.concatenate((window_fd, (float(0),)))
+                    fds_all.append(fd)
+                    
+                    savefeat(childDir, num_win, fd)
+
+#                    fds.append(window_fd)
+#                    labels.append(float(0))
+
+    random.shuffle(fds_all)
+    fds = np.numpy(fds_all)[:, :-1]
+    labels = np.numpy(fds_all)[:, -1]
     new_clf = trainSvm(fds, labels)
 #-----------------------------------------------------------------------------
     joblib.dump(new_clf, new_model_path)
